9ea23efc75a885470773c2ce5ac1780c15905b03
Santoliver <josiasosantosoli@gmail.com>
2018-02-22 00:37:29 -0300

    alterei para udemy

--- index.php
+++ index.php
@@ -3,11 +3,6 @@
 <<<<<<< HEAD
 echo "Hello Work 456";
 
-echo "ola udemy";
-=======
-echo "Hello Work123";
-
-echo "Ola Hcode";
->>>>>>> b813698ce2cac22890d065cd3ae1a65b20a3fa91
+echo "ola udemy e Hcode";
 
 ?>
